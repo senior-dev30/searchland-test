dbda8204e7d68d8f11c0873d21d8b34851de7ae7
yamamoto-ichiro <yamamoto.ichiro92@gmail.com>
2022-05-17 17:50:26 +0100

    Install storybook and ordering library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import './App.css';
+import SvgZone from './components/images/SvgZone';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -10,12 +10,18 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
+        <SvgZone />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
